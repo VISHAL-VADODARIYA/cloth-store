65de356cef05e539ca6635712e9984815e24ec0d
vishal-vadodariya <vadodariyavishal2@gmail.com>
2023-05-16 14:50:36 +0530

    change

--- src/components/UI/Navbar.js
+++ src/components/UI/Navbar.js
@@ -96,7 +96,7 @@ export default function Navbar({
     <>
       <nav class="bg-pink-700 shadow-xl border-gray-200">
         <div class="max-w-screen-2xl md:flex md:flex-wrap items-center justify-around mx-auto p-4">
-          <div className="flex justify-around">
+          <div className="flex justify-between">
             <div>
               <a href="#" class="flex items-center">
                 <span class="text-white self-center text-3xl font-semibold whitespace-nowrap dark:text-white">
@@ -133,101 +133,96 @@ export default function Navbar({
           </div>
 
           <div
-            class={
-              miniToggleMenu &&
-              " w-full md:block md:w-auto hidden flex md:grid "
-            }
+            class={miniToggleMenu && " w-full md:block md:w-auto hidden"}
             id="navbar-default"
           >
-            <div>
-              <ul class="font-medium flex flex-col p-4 md:p-0 mt-4 rounded-lg md:flex-row md:space-x-8 md:mt-0">
-                <li>
-                  <Link to="/">
-                    <a
-                      href="#"
-                      class="block py-1 pl-3 pr-3 text-white rounded hover:bg-white hover:text-pink-700"
-                      aria-current="page"
-                    >
-                      Home
-                    </a>
-                  </Link>
-                </li>
-                <li>
-                  <Link to="/Menswear">
-                    <a
-                      href="#"
-                      class="block py-1 pl-3 pr-3 text-white rounded hover:bg-white hover:text-pink-700"
-                    >
-                      Men's Wear
-                    </a>
-                  </Link>
-                </li>
-                <li>
-                  <Link to="/Womenswear">
-                    <a
-                      href="#"
-                      class="block py-1 pl-3 pr-3 text-white rounded hover:bg-white hover:text-pink-700"
-                    >
-                      Women's Wear
-                    </a>
-                  </Link>
-                </li>
-                <li>
-                  <Link to="/Jewelery">
-                    <a
-                      href="#"
-                      class="block py-1 pl-3 pr-3 text-white rounded hover:bg-white hover:text-pink-700"
-                    >
-                      Jewellery
-                    </a>
-                  </Link>
-                </li>
-                <li>
-                  <Link to="/Electronics">
-                    <a
-                      href="#"
-                      class="block py-1 pl-3 pr-3 text-white rounded hover:bg-white hover:text-pink-700"
-                    >
-                      Electronics
-                    </a>
-                  </Link>
-                </li>
-              </ul>
-            </div>
-            <div className="flex justify-center">
-              {login?.username && (
-                <button
-                  id="loginButton"
-                  className="invisible md:visible px-8 py-1 bg-white rounded text-pink-700 hover:bg-pink-200"
-                >
-                  {login.username}
-                </button>
-              )}
+            <ul class="font-medium flex flex-col p-4 md:p-0 mt-4 rounded-lg md:flex-row md:space-x-8 md:mt-auto">
+              <li>
+                <Link to="/">
+                  <a
+                    href="#"
+                    class="block py-1 pl-3 pr-3 text-white rounded hover:bg-white hover:text-pink-700"
+                    aria-current="page"
+                  >
+                    Home
+                  </a>
+                </Link>
+              </li>
+              <li>
+                <Link to="/Menswear">
+                  <a
+                    href="#"
+                    class="block py-1 pl-3 pr-3 text-white rounded hover:bg-white hover:text-pink-700"
+                  >
+                    Men's Wear
+                  </a>
+                </Link>
+              </li>
+              <li>
+                <Link to="/Womenswear">
+                  <a
+                    href="#"
+                    class="block py-1 pl-3 pr-3 text-white rounded hover:bg-white hover:text-pink-700"
+                  >
+                    Women's Wear
+                  </a>
+                </Link>
+              </li>
+              <li>
+                <Link to="/Jewelery">
+                  <a
+                    href="#"
+                    class="block py-1 pl-3 pr-3 text-white rounded hover:bg-white hover:text-pink-700"
+                  >
+                    Jewellery
+                  </a>
+                </Link>
+              </li>
+              <li>
+                <Link to="/Electronics">
+                  <a
+                    href="#"
+                    class="block py-1 pl-3 pr-3 text-white rounded hover:bg-white hover:text-pink-700"
+                  >
+                    Electronics
+                  </a>
+                </Link>
+              </li>
+            </ul>
+          </div>
+          <div className="flex justify-center">
+            {login?.username && (
+              <button
+                id="loginButton"
+                className=" md:block hidden px-8 py-1 bg-white rounded text-pink-700 hover:bg-pink-200"
+              >
+                {login.username}
+              </button>
+            )}
 
-              <div className="m-2"> </div>
+            <div className="m-2"> </div>
 
-              {login.isLogin && (
-                <button
-                  className="px-8 py-1 bg-white rounded text-pink-700 hover:bg-pink-200"
-                  onClick={Buy}
-                >
-                  Cart{" "}
-                  {login.isLogin && (
-                    <span className="border px-1 rounded-full border-0 bg-pink-700 text-white">
-                      {Object.keys(cartData).length}
-                    </span>
-                  )}
-                </button>
-              )}
-              <div className="m-2"> </div>
+            {login.isLogin && (
+              <button
+                className="px-8 py-1 bg-white rounded text-pink-700 hover:bg-pink-200"
+                onClick={Buy}
+              >
+                Cart{" "}
+                {login.isLogin && (
+                  <span className="border px-1 rounded-full border-0 bg-pink-700 text-white">
+                    {Object.keys(cartData).length}
+                  </span>
+                )}
+              </button>
+            )}
+            <div className="m-2"> </div>
 
-              {login.isLogin && (
-                <button
-                  className="px-3 py-1 bg-white rounded  text-pink-700 hover:bg-pink-200 fa fa-sign-out"
-                  onClick={logout}
-                ></button>
-              )}
-            </div>
+            {login.isLogin && (
+              <button
+                className="px-3 py-1 bg-white rounded  text-pink-700 hover:bg-pink-200 fa fa-sign-out"
+                onClick={logout}
+              ></button>
+            )}
           </div>
         </div>
       </nav>
